92e81004bbac7851cbdbe9591428b2676624b98e
pawsmeow <161309672+pawsmeow@users.noreply.github.com>
2024-10-23 20:51:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const cards = document.querySelector('.cards');
 const content = document.querySelector('.content');
 const backButton = document.getElementById('backButton');
 const nextButton = document.getElementById('nextButton');
-let selectedCard = null;
 
 // Запускаем анимацию через 0.5 секунды после загрузки
 setTimeout(() => { // Поднимаем заголовок вверх
@@ -15,34 +14,7 @@ setTimeout(() => { // Поднимаем заголовок вверх
 }, 500);
 
 // Обработчик кликов по карточкам
-document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
-        // Снимаем выделение с ранее выбранной карточки
-        if (selectedCard) {
-            selectedCard.classList.remove('selected');
-        }
 
-        // Выделяем новую карточку
-        let selectedCard = card;
-        let type = selectedCard.dataset.type;
-        let price = selectedCard.dataset.price;
-        selectedCard.classList.add('selected');
-
-        // Показываем кнопку "Далее" только после выбора карточки
-        nextButton.style.display = 'block';
-    });
-});
-
-// Обработчик нажатия кнопки "Далее"
-nextButton.addEventListener('click', () => {
-    if (selectedCard) {
-        // Показать контент и скрыть карточки
-        cards.style.display = 'none';
-        content.style.display = 'block';
-        backButton.style.display = 'block'; // Показываем кнопку "Назад"
-        nextButton.style.display = 'none'; // Скрываем кнопку "Далее"
-    }
-});
 
 // Обработчик нажатия кнопки "Назад"
 backButton.addEventListener('click', () => {
